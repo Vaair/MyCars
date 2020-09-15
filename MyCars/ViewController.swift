2f0a557af48b9eaa10cce58f6540861a252c7494
Vaair <valeria.tarasenko.a@gmail.com>
2020-09-15 13:48:08 +0300

    test

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Ivan Akulov on 08/02/20.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    var context: NSManagedObjectContext! 
+    
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        
+    }
+    
+    private func insertDataFrom(selectedCar car: Car){
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timeDriven)"
+        
+        lastTimeStartedLabel.text = " Last time started: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.tintColor = car.tintColor as? UIColor
+    }
+    
+    private func getDataFromFile() {
+        
+        //проверка есть ли данные в базе данных (hardcore)
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        
+        //
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+            let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        
+        for dictionary in dataArray {
+            guard let entity = NSEntityDescription.entity(forEntityName: "Car", in: context) else { return }
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timeDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            
+            guard let imageName = carDictionary["imageName"] as? String,
+                let image = UIImage(named: imageName) else { return }
+            let imageData = image.pngData()
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float]{
+                car.tintColor = getColor(colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(_ colorDictionary: [String : Float]) -> UIColor{
+        guard let red = colorDictionary["red"],
+            let green = colorDictionary["green"],
+            let blue = colorDictionary["blue"] else { return UIColor() }
+        
+        return UIColor(red: CGFloat(red / 255) , green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDataFromFile()
+        //нужно вызывать только раз user defalt
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            let car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+ 
+    }
+    
+}
+
